66b936406858b5832e11b349772416578dc9aca9
Aman choudhary <81174648+Amanchoudhary192002@users.noreply.github.com>
2025-07-25 22:15:02 +0530

    file change

--- js/click.js
+++ js/click.js
@@ -1,17 +1,18 @@
 function login() {
-    alert("Please Login ");
-};
+    alert("Please Login");
+    };
+  
+    var deleteLinks = document.querySelectorAll('.delete');
 
-var deleteLinks = document.querySelectorAll('.delete');
-
-for (var i = 0; i < deleteLinks.length; i++) {
-    deleteLinks[i].addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var choice = confirm(this.getAttribute('data-confirm'));
-
-        if (choice) {
+    for (var i = 0; i < deleteLinks.length; i++) {
+      deleteLinks[i].addEventListener('click', function(event) {
+          event.preventDefault();
+    
+          var choice = confirm(this.getAttribute('data-confirm'));
+    
+          if (choice) {
             window.location.href = this.getAttribute('href');
-        }
-    });
-}
+          }
+      });
+    }
+    
